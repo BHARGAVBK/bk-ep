a6b6dc18d706abf68d0cf8a5d4a4cdca78878d49
Bhargavkumar <89638763+BHARGAVBK@users.noreply.github.com>
2022-08-23 10:30:07 +0530

    Initial commit

---
+++ ethers-simple-storage-fcc/index.js
@@ -0,0 +1,13 @@
+// const process = require("process");
+
+// async function main() {
+//   console.log("Wallet");
+//   let varirable = 5;
+//   console.log(varirable);
+// }
+// main()
+//   .then(() => process.exit(0))
+//   .catch((error) => {
+//     console.error(error);
+//     process.exit(1);
+//   });
