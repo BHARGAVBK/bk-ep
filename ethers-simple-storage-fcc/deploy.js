72dad6e22b7814ae23ad9b9e23c71e23ab4861f7
Bhargavkumar <89638763+BHARGAVBK@users.noreply.github.com>
2022-08-23 17:13:36 +0530

    Update deploy.js

--- ethers-simple-storage-fcc/deploy.js
+++ ethers-simple-storage-fcc/deploy.js
@@ -7,13 +7,14 @@ const encryptedJson = fs.readFileSync("./.encryptedKey.json", "utf-8");
 //http://127.0.0.1:7545
 // connect to local blockchain
 const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-let wallet = new ethers.Wallet.fromEncryptedJsonSync(
-  encryptedJson,
-  process.env.PRIVATE_KEY_PASSWORD
-);
-wallet = await wallet.connect(provider);
-//const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-//contract application binary interface compiled file
+// let wallet = new ethers.Wallet.fromEncryptedJsonSync(
+//   encryptedJson,
+//   process.env.PRIVATE_KEY_PASSWORD
+// );
+// wallet = await wallet.connect(provider);
+
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+// contract application binary interface compiled file
 const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8");
 //contract binary compiled file
 const binary = fs.readFileSync(
