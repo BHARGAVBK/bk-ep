434ad16c13373b4a523c0af91a1dcc16492765ac
Bhargavkumar <89638763+BHARGAVBK@users.noreply.github.com>
2022-08-26 17:34:25 +0530

    last update

--- NFT-Market/contracts/NFTMarketplace.sol
+++ NFT-Market/contracts/NFTMarketplace.sol
@@ -28,27 +28,35 @@ contract NFTMarket is ERC721URIStorage {
     }
 
     mapping (uint256 => listedToken) private idToListedToken;
-
+    
+    //helper function
     function updateListPrice(uint256 _listPrice) public payable{
 
-        require(owner == msg.sender,"Only woner can update the listing price");
+        require(owner == msg.sender,"Only owner can update the listing price");
         listPrice = _listPrice;
     }
+    
+    //helper function
     function getListPrice()public view returns(uint256) { return listPrice; }
-
+    
+    //helper function
     function getlatestIdToListedToken() public view returns(listedToken memory){
         uint256 currentTokenId = _tokenIds.current();
         return idToListedToken[currentTokenId];
     }
 
+    //helper function 
+    //       get listedToken for tokenId
     function getListedForTokenId(uint256 _tokenId)public view returns(listedToken memory){
         return idToListedToken[_tokenId];
     }
 
+    //helper function
     function getCurrentToken()public view returns(uint256){
         return _tokenIds.current();
     }
 
+    // main func to create token
     function createToken(string memory _tokenURI,uint256 _price)public payable{
         require(msg.value == listPrice,"Send enough ether to list!");
         require(price < 0,"Make sure price isn't negative!");
@@ -64,7 +72,7 @@ contract NFTMarket is ERC721URIStorage {
         return currentTokenId;
 
     }
-
+    
     function createListedtoken(uint256 _tokenId, uint256 _price)private{
         idToListedToken[_tokenId] = listedToken(
             _tokenId,
@@ -76,4 +84,33 @@ contract NFTMarket is ERC721URIStorage {
         _transfer(msg.sender,address(this),_tokenId);
     }
     
+    function getAllNFTs()public view returns(listedToken[] memory){
+        uint nftCount = _tokenIds.current();
+        listedToken[] memory tokens = new listedToken[](nftCount);
+        
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < nftCount; i++) 
+        {
+            uint currentId = i+1;
+            listedToken storage currentItem = idToListedToken[currentId];
+            tokens[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        return tokens;
+    }
+
+    function getMyNFTs() public view returns(listedToken[] memory){
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < totalItemCount; i++) {
+            if (idToListedToken[i+1].owner == msg.sender || idToListedToken[i+1].seller == msg.sender)
+            {
+                itemCount + 1;
+            }
+            
+        }
+    }
 }
